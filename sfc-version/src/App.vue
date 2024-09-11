917ad08c6949e2ab71ecd8909f8fc98300060da2
spansini <spansini@squad.fr>
2024-09-11 16:07:08 +0200

    [PROJET-1] Init source files of App - SFC version

---
+++ sfc-version/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+:root {
+ --font-400: 400;
+ --font-500: 500;
+ --font-700: 700;
+
+ --c-black: #000;
+ --c-black75: rgba(0, 0, 0, .75);
+ --c-black50: rgba(0, 0, 0, .5);
+ --c-grey: #828282;
+ --c-second-grey: #e6e6e6;
+ --c-third-grey: #d9d9d9;
+ --c-light-grey: #f7f7f7;
+ --c-white: #fff;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Inter", sans-serif;
+}
+
+</style>
