62a95188b3a8549aabfbe96bc63fa5e035638759
spansini <spansini@squad.fr>
2024-09-11 16:32:23 +0200

    [PROJET-1] SFC-version: Add navbar component & first article

--- sfc-version/src/App.vue
+++ sfc-version/src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-
+  import { ref } from 'vue'
+  import Header from './components/Header.vue'
 </script>
 
 <template>
-  
+  <Header />
+  <main>
+    <article>
+      <h1>Landing page title</h1>
+      <p>Subheading that sets up context, shares more info about the website, or generally gets people psyched to keep scrolling.</p>
+      <button class="btn_big">Button</button>
+      <img src="./assets/Hero Image.png" alt="Hero Landingpage">
+    </article>
+  </main>
 </template>
 
 <style lang="scss">
@@ -30,4 +39,58 @@ body {
   font-family: "Inter", sans-serif;
 }
 
+#app {
+  max-width: 1440px;
+  margin: 0 auto;
+  background: var(--c-white);
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+}
+
+/* MAIN: article */
+article {
+  padding: 0 80px;
+  h1 {
+    font-size: 64px;
+    color: var(--c-black);
+    line-height: auto;
+    letter-spacing: -2px;
+  }
+  p {
+    margin-top: 24px;
+    font-size: 24px;
+    color: var(--c-black75);
+    font-weight: 400;
+    max-width: 844px;
+  }
+  button {
+    margin: 40px 0 80px; 
+    &.btn_big {
+      font-size: 24px;
+      padding: 20px 32px;
+    }
+  }
+  img {
+    max-width: 1280px;
+    height: auto;
+  }
+}
+
+/* Global style for button */ 
+button {
+  background: var(--c-black);
+  color: var(--c-white);
+  border-radius: 8px;
+  border: none;
+  cursor: pointer;
+  transition: .2s ease-out;
+  &:hover {
+    background: var(--c-third-grey);
+    color: var(--c-black75);
+    transition: .3s ease-in;
+  }
+  &.btn_std {
+    font-size: 16px;
+    padding: 14px 24px;
+  }
+}
 </style>
---
+++ sfc-version/src/components/Header.vue
@@ -0,0 +1,81 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const navList = ref([
+    {id: 0, text: 'Page One'},
+    {id: 1, text: 'Page Two'},
+    {id: 2, text: 'Page Three'},
+    {id: 2, text: 'Page Four'}
+  ])
+
+</script>
+
+<template>
+
+  <header>
+    <div class="logo">Site name</div>
+    <nav>
+      <ul>
+        <li v-for="itemNav in navList" :key="itemNav.id">{{ itemNav.text }}</li>
+      </ul>
+      <button class="btn_std" type="button>">Button</button>
+    </nav>
+  </header>
+
+</template>
+
+<style lang="scss">
+  header {
+  height: 164px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 80px;
+  
+  .logo {
+    font-size: 20px;
+    color: var(--c-black);
+    font-weight: 500;
+  }
+  nav {
+    display: flex;
+    align-items: center;
+    ul {
+      list-style: none;
+      display: flex;
+      gap: 48px;
+      li {
+        font-size: 20px;
+        font-weight: 500;
+        color: var(--c-black);
+        cursor: pointer;
+        position: relative;
+        transition: .2s ease-out;
+        &:hover {
+          color: var(--c-black50);
+          transition: .3s ease-in;
+          &::after {
+            background: var(--c-black50);
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 2px;
+            left: 0;
+            bottom: -10px;
+            transition: .3s ease-in;
+          }
+        }
+        &::after {
+          width: 0;
+          position: absolute;
+          content: '';
+          transition: .2s ease-out;
+        }
+      }
+    }
+    button {
+      margin-left: 48px;
+    }
+  }
+}
+</style>
